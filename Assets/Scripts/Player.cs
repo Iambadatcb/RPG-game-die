15561db290999c3c278c4c124921c5228a64bf19
Iambadatcb <gviduks.cepuliuks@gmail.com>
2025-09-03 12:55:21 +0300

    movementr

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,26 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public TextMeshProUGUI lvl;
-    private int xp = 0;
+    public TextMeshProUGUI xpForLvl;
+    public Transform player;
+    public float speed = 1.5f;
+    private float normalSpeed;
+    private float xp = 0;
+    private float forLvl = 100;
+    public float speedMulti = 4f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        xpForLvl.text = forLvl.ToString();   
+        normalSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        var x = Input.GetAxisRaw("Horizontal");
+        var z = Input.GetAxisRaw("Vertical");
         xp++;
-        if (xp == 100)
+        if (xp >=   forLvl)
         {
-            xp -=100;
+            xp -=forLvl;
+            forLvl = forLvl*1.25f;
+
         }
-        lvl.text = xp.ToString();
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            
+            speed = speedMulti;
+        }
+        else
+        {
+            speed = normalSpeed;
+        }
+        lvl.text = xp.ToString("F2");
+        xpForLvl.text = forLvl.ToString("F2");
+        player.position += new Vector3(x,0,z).normalized * speed * Time.deltaTime;
+    }
+    void OnCollision3D(Collider collider)
+    {
+        if(collider.CompareTag("Mob"))
+        {
+            collider.gameObject.SetActive(false);   
+        }
     }
 }
